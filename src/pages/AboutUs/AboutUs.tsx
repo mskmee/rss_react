65b0e7dbe241a447f5db799e4b4d66a67b91ca28
Maksim Maksimenko <mskmee2@gmail.com>
2023-03-10 10:32:03 +0200

    refactor: styles module in about us and error page

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -1,11 +1,10 @@
-import SearchBar from '../../components/SearchBar/SearchBar';
 import React, { Component } from 'react';
 import styles from './AboutUs.module.css';
 
 export default class AboutUs extends Component {
   render() {
     return (
-      <div>
+      <div className={styles.wrapper}>
         <h1>About Us</h1>
       </div>
     );
--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -4,7 +4,7 @@ import styles from './NotFoundPage.module.css';
 export default class NotFoundPage extends Component {
   render() {
     return (
-      <div>
+      <div className={styles.wrapper}>
         <h1>This page is not found: 404</h1>
       </div>
     );
