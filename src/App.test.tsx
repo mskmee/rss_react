071f88af3771d53ba0df109ddf351ce4a69992e5
Maksim Maksimenko <mskmee2@gmail.com>
2023-03-28 08:39:32 +0300

    fix: app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import { App } from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { App } from './App';
+import { store } from './state/store';
 
 describe('when rendered', () => {
   it('should have header', () => {
     render(
       <BrowserRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     );
     expect(screen.queryByRole('header')).toBeTruthy();
